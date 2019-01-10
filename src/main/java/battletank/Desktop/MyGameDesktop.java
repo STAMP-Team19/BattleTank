c10f07775fba37ff6a101d540f0e86886d6607d7
trolund@gmail.com <vxh77kdp>
2019-01-10 14:46:16 +0100

    setup game, tilføjelse af input klasse, begyndt import af map

--- src/main/java/battletank/Desktop/MyGameDesktop.java
+++ src/main/java/battletank/Desktop/MyGameDesktop.java
@@ -8,6 +8,13 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 public class MyGameDesktop {
     public static void main (String[] args) {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.fullscreen = false;
+        config.foregroundFPS = 30;
+       // config.backgroundFPS = 24;
+        config.height = 800;
+        config.width = 800;
+        config.resizable = true;
+        config.title = "BattelTank";
         new LwjglApplication(new MyGame(), config);
     }
 }
---
+++ src/main/java/battletank/controls/controllers/Input.java
@@ -0,0 +1,46 @@
+package battletank.controls.controllers;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.InputProcessor;
+
+public class Input extends ApplicationAdapter implements InputProcessor {
+    @Override
+    public boolean keyDown(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int i) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char c) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int i, int i1, int i2, int i3) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int i, int i1, int i2) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int i, int i1) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int i) {
+        return false;
+    }
+}
