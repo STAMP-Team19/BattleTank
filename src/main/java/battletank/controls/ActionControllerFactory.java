6b38e77231c0906efefb5e22307ff32a7c2dde2c
Arvid Langsø <arvid.k.langsoe@live.dk>
2019-01-10 08:50:33 +0100

    Unused imports

--- src/main/java/battletank/controls/ActionControllerFactory.java
+++ src/main/java/battletank/controls/ActionControllerFactory.java
@@ -1,16 +1,18 @@
 package battletank.controls;
 
-import battletank.controls.Action;
-import battletank.controls.ActionInfo;
 import battletank.controls.controllers.ActionController;
 import battletank.controls.controllers.MovementController;
-
 
 public class ActionControllerFactory {
 
 
     public ActionController constructAction(ActionInfo action){
-
-        return new MovementController(action);
+        switch (action.getAction()){
+            case Move_Backward:
+            case Move_Forward:
+            case Move_Stop:
+                return new MovementController(action);
+        }
+        return null;
     }
 }
