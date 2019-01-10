57b7b20cb75da70624a264f492600688ce5fcdeb
Peter <peterxp@hotmail.fr>
2019-01-10 14:28:11 +0100

    Hotfix

--- src/main/java/battletank/lobby/Lobby.java
+++ src/main/java/battletank/lobby/Lobby.java
@@ -4,5 +4,5 @@ import java.util.ArrayList;
 
 public class Lobby {
 
-    public final String[] players = {"Arvid", "Peter", "Troels", "Simon", "Mads"};
+    public final static String[] players = {"Arvid", "Peter", "Troels", "Simon", "Mads"};
 }
