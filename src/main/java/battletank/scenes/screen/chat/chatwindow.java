7e559d8f1f23ba0b2ebb38696cc05430280d0287
Mads <Mads@stege.eu>
2019-01-18 12:57:31 +0100

    Added functionality to added messages to chat area.

--- src/main/java/battletank/scenes/screen/chat/chatwindow.java
+++ src/main/java/battletank/scenes/screen/chat/chatwindow.java
@@ -5,39 +5,76 @@ import spaces.game.connect.chat.IChatListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class chatwindow implements IChatListener, Runnable {
-    private JTextPane textFieldChat;
+public class chatwindow implements IChatListener, Runnable, KeyListener {
     private JTextField textFieldInput;
-    private JButton sendMessageButton;
+    private JButton buttonSendMessage;
     private JPanel JPanelMain;
+    private JTextArea textAreaChat;
 
     private String ip, username;
     private ChatMsgManager manager;
+    Dimension border = new Dimension(500, 400);
 
 
     public chatwindow(String username, String ip) {
         this.username = username;
         this.ip = ip;
 
+        buttonSendMessage.addKeyListener(this);
 
+
+        buttonSendMessage.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String msgInput = textFieldInput.getText();
+                textAreaChat.append(username + ": " + msgInput + "\n");
+                textFieldInput.setText("");
+            }
+        });
     }
+
 
     @Override
     public void notifyNewMessage(String username, String message) {
         System.out.println("TODO IMPLEMENT");
     }
 
+
     @Override
     public void run() {
         JFrame frame = new JFrame("chatwindow");
         frame.setContentPane(new chatwindow(username, ip).JPanelMain);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setPreferredSize(new Dimension(500, 400));
+        frame.setPreferredSize(border);
+
+
         frame.pack();
         frame.setVisible(true);
 
         manager = new ChatMsgManager(username, this, ip);
         manager.sendMessage(username, "Test message: Chat started correctly.");
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            System.out.println("Enter pushed");
+        }
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
     }
 }
