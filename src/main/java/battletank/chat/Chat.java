3e43a4795c82511fc98aeb29ab0c4780c4c6afc9
Peter <peterxp@hotmail.fr>
2019-01-18 11:17:42 +0100

    MessagesManager implemented

--- src/main/java/battletank/chat/Chat.java
+++ src/main/java/battletank/chat/Chat.java
@@ -1,4 +1,29 @@
 package battletank.chat;
 
+import org.jspace.SequentialSpace;
+
 public class Chat {
+    private SequentialSpace chatspace;
+
+    public Chat(SequentialSpace chatspace) {
+        this.chatspace = chatspace;
+    }
+
+
+}
+
+class MessageListener implements Runnable{
+
+    SequentialSpace chatspace;
+
+    public MessageListener(SequentialSpace chatspace) {
+        this.chatspace = chatspace;
+    }
+
+    @Override
+    public void run() {
+        while(true){
+
+        }
+    }
 }
--- src/main/java/spaces/game/connect/chat/ChatMsgManager.java
+++ src/main/java/spaces/game/connect/chat/ChatMsgManager.java
@@ -1,22 +1,52 @@
 package spaces.game.connect.chat;
 
+import battletank.lobby.PlayerInfo;
+import com.google.gson.Gson;
+import org.jspace.RemoteSpace;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class ChatMsgManager implements IChatMsgManager {
 
-    private IChatListener listener;
-    private String ip;
+    private RemoteSpace chatspace;
 
     public ChatMsgManager(String username, IChatListener listener, String ip) {
-        this.listener = listener;
-        this.ip = ip;
+        String uri = "tcp://"+ip+":9003/chat?keep";
+
+        try {
+            chatspace = new RemoteSpace(uri);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void sendMessage(String username, String message) {
         System.out.println("Message '"+message+"' from "+username+" sent.");
+        try {
+            chatspace.put(username, message);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void sendInformationEvent(String event) {
         System.out.println("Event Message "+event+" sent.");
+        try {
+            chatspace.put("[EVENT]", event);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void updatePlayers(ArrayList<PlayerInfo> players) {
+        try {
+            chatspace.put("UPDATE", new Gson().toJson(players));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
